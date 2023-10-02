1d091ccf59ee532e1d1e9eeca9440f7100c6d78c
Jeff Page <83289672+jeffmahay@users.noreply.github.com>
2023-10-02 16:54:05 -0600

    Update main.py
    fixed connections

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ import os
 
 # Project Settings, Service Accounts, Create Service Account, and then
 # Generate New Private Key
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "YOUR_FILE_NAME_HERE.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "storyboard-deb58-firebase-adminsdk-6ih4y-a6baa46db0.json"
 # Use the application default credentials. The projectID is obtianed
 # by going to Project Settings and then General.
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred, {
- 'projectId': 'YOUR_PROJECT_NAME_HERE',
+ 'projectId': 'storyboard-deb58',
 })
 # Get reference to database
 db = firestore.client()
+
+
+
 
 
