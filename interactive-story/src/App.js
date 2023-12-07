c22d81ea1ff624d6723511a452b81808f7d35a59
narah10 <kwa18001@byui.edu>
2023-12-06 18:17:38 -0700

    Updated

--- interactive-story/src/App.js
+++ interactive-story/src/App.js
@@ -6,6 +6,7 @@ import FAQ from './components/faq/faq';
 import Contacts from './components/contact/contact';
 import Stories from './components/stories/stories';
 import StoryOne from './components/stories/story1';
+import StoryThree from './components/stories/story3';
 
 function App() {
 
@@ -29,6 +30,7 @@ function App() {
         <Route path='/Contacts' element={<Contacts />}/>
         <Route path='/Stories' element={<Stories />}/>
         <Route path='/StoryOne' element={<StoryOne documentId={documentId} onButtonPress={handleButtonPress}/>}/>
+        <Route path='/StoryThree' element={<StoryThree documentId={documentId} onButtonPress={handleButtonPress}/>}/>
       </Routes>
     </div>
     
---
+++ interactive-story/src/components/stories/story3.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import { db } from '../../firestore';
+import storyStyles from "./storyStyles.module.css"
+import home from "../homepage/home.module.css"
+import NavBar from '../navbar/nav';
+import Background from '../background';
+import { Link } from 'react-router-dom';
+
+function StoryThree({ documentId, onButtonPress }) {
+  const [entry, setEntry] = useState(null);
+
+  useEffect(() => {
+    const docRef = db.collection('story').doc(documentId);
+
+    const unsubscribe = docRef.onSnapshot((snapshot) => {
+      if (snapshot.exists) {
+        setEntry({
+          id: snapshot.id,
+          ...snapshot.data(),
+        });
+      } else {
+        console.log("ERROR: Document does not exist");
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [documentId]); 
+
+  if (!entry) {
+    return (
+      <div className={home.container}>
+      <NavBar/>
+      <div>
+        <Background>
+        <div>Loading Story...</div>
+        </Background>
+        </div>
+    </div>
+  )
+}
+
+  const handleButtonPress = async(buttonId) => {
+    if (entry[buttonId]) {
+        onButtonPress(entry[buttonId]);
+      } 
+      else {
+        console.log(`Document ID not found in ${buttonId} field.`);
+      }
+   };
+
+   const buttons = Object.keys(entry)
+    .filter((key) => key.startsWith('Option') || key === 'Back' || key.startsWith('Game')) // Filter button fields
+    .map((key) => ({ id: key, label: entry[key] })); // Map to an array of objects
+
+
+  return (
+
+    <div className={home.container}>
+      <NavBar/>
+      <div>
+        <Background>
+          <div className={storyStyles.storyContainer}>
+            <div className={storyStyles.headingContainer}>
+                <h1>The Enchanted Forest Adventure</h1>
+                <Link to="/Stories" style={{textAlign:'right'}}>Exit Story</Link>
+            </div>
+            <div className={storyStyles.storyInnerContainer}>
+              <div className={storyStyles.storyData}>
+                <h2 className={storyStyles.title}>{entry.title}</h2>
+                <p>{entry.option1}</p>
+                <p className={storyStyles.description}>{entry.desc}</p>
+                <div className={storyStyles.content}>
+                    <div className={storyStyles.buttonStyle}>
+                    {buttons.map((button) => (
+                        <button className={storyStyles.buttons} key={button.id} onClick={() => handleButtonPress(button.id)}>
+                        {button.id}
+                        </button>
+                    ))}
+                    </div>
+                    <div className={storyStyles.imageContainer}>
+                        <img className={storyStyles.images} src={entry.image}/>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+                </Background>
+        </div>
+    </div>
+  );
+}
+
+export default StoryThree;
